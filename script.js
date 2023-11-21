93468cb83e055098606d351468743c74b17e5def
DentArthur94 <73761376+DentArthur94@users.noreply.github.com>
2023-11-21 21:55:06 +0100

    Add function for computer choice

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 "use strict"
 
+// The computer choice is randomly generated
+function getComputerChoice() {
+    let randomSelection = Math.floor(Math.random() *3);
+    
+    switch (randomSelection) {
+        case 0:
+            return 'Rock';
+        case 1:
+            return 'Papper';
+        case 2:
+            return 'Scissor'; 
+    } 
+}
+
+// As I user I want to input my choice, case insenstive
+
+
+
+//I want to play a round of RPS against the computer
+
+//The winner is displayed after 5 rounds
+
